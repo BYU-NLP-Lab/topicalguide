b645c2d1b484a1453ba2756016bf36ff375a07f1
Josh Hansen <joshuaaaronhansen@gmail.com>
2010-12-21 20:28:19 -0700

    More useful output in the state of the union data extractor

--- build/state_of_the_union/extract_sotua_documents.py
+++ build/state_of_the_union/extract_sotua_documents.py
@@ -20,6 +20,8 @@ def lines_to_string(lines):
 def extract_state_of_the_union(chron_list_filename, addresses_filename, dest_dir):
     print "extract_state_of_the_union({0},{1},{2})".format(chron_list_filename, addresses_filename, dest_dir)
     titles = [m.group('title') for m in metadata(chron_list_filename)]#data_dir+'/chronological_list.wiki')]
+    print 'Addresses in index: ' + str(len(titles))
+    extracted_count = 0
     if not os.path.exists(dest_dir): os.mkdir(dest_dir)
     
     current_speech_title = None
@@ -32,6 +34,7 @@ def extract_state_of_the_union(chron_list_filename, addresses_filename, dest_dir
                 w.write(lines_to_string(lines))
                 lines = []
                 print 'Extracted "{0}"'.format(current_speech_title)
+                extracted_count += 1
             current_speech_title = line
         else:
             lines += [line]
@@ -39,6 +42,9 @@ def extract_state_of_the_union(chron_list_filename, addresses_filename, dest_dir
     w = codecs.open(dest_dir+'/'+current_speech_title.replace(' ','_')+'.txt','w','utf-8')
     w.write(lines_to_string(lines))
     print 'Extracted "{0}"'.format(current_speech_title)
+    extracted_count += 1
+    print 'Addresses extracted: ' + str(extracted_count)
+
 
 
 if __name__=='__main__':
