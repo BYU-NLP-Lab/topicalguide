BYU-NLP-Lab/topicalguide

commit 295b454bab2449b834863d84619ad45549181d34

Really big commit, but oh well

topic_modeling/visualize/topics/ajax.py
@@ -22,22 +22,27 @@
 # contact the Copyright Licensing Office, Brigham Young University, 3760 HBLL,
 # Provo, UT 84602, (801) 422-9339 or 422-3821, e-mail copyright@byu.edu.
 
-
 from django.http import HttpResponse
 from django.utils import simplejson
+from django.db.models.aggregates import Min
 
 from topic_modeling.visualize.common import FilterForm
 from topic_modeling.visualize.common import paginate_list
-from topic_modeling.visualize.models import Analysis
+from topic_modeling.visualize.models import Analysis, DocumentTopicWord
 from topic_modeling.visualize.models import Attribute
 from topic_modeling.visualize.models import Topic
-from topic_modeling.visualize.models import TopicName
-from topic_modeling.visualize.models import TopicNameScheme
+from topic_modeling.visualize.models import TopicGroup
+from topic_modeling.visualize.models import TopicGroupTopic
+from topic_modeling.visualize.models import TopicWord
+from topic_modeling.visualize.models import DocumentTopic
+from topic_modeling.visualize.models import AttributeValueTopic
 from topic_modeling.visualize.topics.common import top_values_for_attr_topic
 from topic_modeling.visualize.topics.common import get_topic_name
 from topic_modeling.visualize.topics.filters import clean_topics_from_session
 from topic_modeling.visualize.topics.filters import get_topic_filter_by_name
 from topic_modeling.visualize.topics.filters import possible_topic_filters
+import sys
+from django.db import transaction
 
 # General and Sidebar stuff
 ###########################
@@ -211,11 +216,127 @@ def update_topic_word_filter(request, dataset, analysis, topic, number, word):
     request.session.modified = True
     return filtered_topics_response(request, dataset, analysis)
 
-
 class AjaxTopic(object):
     def __init__(self, topic, topic_name):
-        self.name = str(topic.number) + ': ' + topic_name
+        self.name = topic_name
         self.number = topic.number
+        try:
+            self.topicgroup = [topic.name for topic
+                               in topic.topicgroup.subtopics]
+        except:
+            self.topicgroup = False
 
+# Topic Groups
+
+def create_topic_group(request, dataset, analysis, name):
+    analysis = Analysis.objects.get(dataset__name=dataset, name=analysis)
+    number = Topic.objects.aggregate(Min('number'))['number__min'] - 1#usually < 0...
+    topic_group = TopicGroup(analysis=analysis, name=name, number=number)
+    topic_group.total_count = 0
+    topic_group.save()
+    return HttpResponse('Group %d:"%s" Created!' % (topic_group.number, topic_group.name))
+
+def remove_topic_group(request, number):
+    number = int(number)
+    topic_group = TopicGroup.objects.get(number=number)
+    topic_group.delete()
+    return HttpResponse('Group %d removed.' % number)
+
+@transaction.commit_manually
+def clear_topic_group(topic_group):
+    topic_group.total_count = 0
+    for topic_word in topic_group.topicword_set.all():
+        topic_word.delete()
+    for doctop in topic_group.documenttopic_set.all():
+        doctop.delete()
+    for avt in topic_group.attributevaluetopic_set.all():
+        avt.delete()
+    for doctopword in topic_group.documenttopicword_set.all():
+        doctopword.delete()
+
+    transaction.commit()
+
+@transaction.commit_manually
+def reaggregate_words(topic_group):
+    words = {}
+    for topic in topic_group.subtopics:
+        for topic_word in topic.topicword_set.all():
+            words[topic_word.word] = words.get(topic_word.word, 0) + topic_word.count
+            topic_group.total_count += topic_word.count
+
+    for word, count in words.iteritems():
+        TopicWord(topic=topic_group, word=word, count=count).save()
+    topic_group.save()
+
+    transaction.commit()
+
+@transaction.commit_manually
+def reaggregate_docs(topic_group):
+    docs = {}
+    for topic in topic_group.subtopics:
+        for dt in topic.documenttopic_set.all():
+            docs[dt.document] = docs.get(dt.document, 0) + dt.count
+
+    for doc, count in docs.iteritems():
+        DocumentTopic(topic=topic_group, document=doc, count=count).save()
+
+    transaction.commit()
+
+@transaction.commit_manually
+def reaggregate_attrs(topic_group):
+    attrs = {}
+    for topic in topic_group.subtopics:
+        for avt in topic.attributevaluetopic_set.all():
+            attrs[avt.value] = attrs.get(avt.value, 0) + avt.count
+
+    for val, count in attrs.iteritems():
+        AttributeValueTopic(topic=topic_group,
+                            value=val,
+                            attribute=val.attribute,
+                            count=count).save()
+
+    transaction.commit()
+
+@transaction.commit_manually
+def reaggregate_doctopicword(topic_group):
+    doctopwords = {}
+    for topic in topic_group.subtopics:
+        for dtw in topic.documenttopicword_set.all():
+            count = doctopwords.get((dtw.topic, dtw.word, dtw.document), 0)
+            doctopwords[dtw.topic, dtw.word, dtw.document] = count + dtw.count
+
+    for keys, count in doctopwords.iteritems():
+        topic, word, doc = keys
+        DocumentTopicWord(topic=topic,
+                          word=word,
+                          document=doc,
+                          count=count).save()
+
+    transaction.commit()
+
+def reaggregate_topicgroup(topic_group):
+    clear_topic_group(topic_group)
+    reaggregate_words(topic_group)
+    reaggregate_docs(topic_group)
+    reaggregate_attrs(topic_group)
+    reaggregate_doctopicword(topic_group)
+
+def add_topic_to_group(request, dataset, analysis, number, addnumber):
+    analysis = Analysis.objects.get(dataset__name=dataset, name=analysis)
+    topic_group = analysis.topic_set.get(number=number).topicgroup
+    topic = analysis.topic_set.get(number=addnumber)
+    if topic not in topic_group.subtopics:
+        TopicGroupTopic(topic=topic, group=topic_group).save()
+        reaggregate_topicgroup(topic_group)
+        return HttpResponse('Added topic %d to group %d' % (topic.number, topic_group.number))
+
+def remove_topic_from_group(request, dataset, analysis, number, addnumber):
+    analysis = Analysis.objects.get(dataset__name=dataset, name=analysis)
+    topic_group = analysis.topic_set.get(number=number).topicgroup
+    topic = analysis.topic_set.get(number=addnumber)
+    if topic in topic_group.subtopics:
+        TopicGroupTopic.objects.get(topic=topic, group=topic_group).delete()
+        reaggregate_topicgroup(topic_group)
+        return HttpResponse('Removed topic %d to group %d' % (topic.number, topic_group.number))
 
 # vim: et sw=4 sts=4

topic_modeling/visualize/topics/common.py
@@ -34,8 +34,12 @@ from topic_modeling.visualize.models import TopicNameScheme
 
 def get_topic_name(topic, name_scheme_id):
     ns = TopicNameScheme.objects.get(id=name_scheme_id)
-    return TopicName.objects.get(topic=topic, name_scheme=ns).name
-
+    name = str(topic.number) + ": "
+    try:
+        name += TopicName.objects.get(topic=topic, name_scheme=ns).name
+    except:
+        name += topic.name
+    return name
 
 def sort_topics(topics, sort_by, session):
     # Because of the way I had to implement metric sorting, and the way that
@@ -55,7 +59,7 @@ def sort_topics(topics, sort_by, session):
         metric_name = sort_by[7:]
         topic_list = list(topics.all())
         topic_list.sort(key=lambda x:
-                -x.topicmetricvalue_set.get(metric__name=metric_name).value)
+                - x.topicmetricvalue_set.get(metric__name=metric_name).value)
         return topic_list
     else:
         raise ValueError("We don't current support ordering by %s" % sort_by)
@@ -75,7 +79,7 @@ def top_values_for_attr_topic(analysis, topic, attribute, order_by='count',
         percent = float(count) / total_count
         values.append(ValueListing(value.value, count, percent))
     if order_by == 'percent':
-        values.sort(key=lambda x: -x.percent)
+        values.sort(key=lambda x:-x.percent)
     return values[:number]
 
 

build/nytimes_twitter/clean_tweets.py
@@ -29,19 +29,20 @@ import re
 
 class TweetCleaner(Cleaner):
     def __init__(self, input_dir, output_dir):
-        super(TweetCleaner,self).__init__(input_dir, output_dir, u"’“”\r\n'-/.,", punctuation+digits.replace('#',''))
-    
+        super(TweetCleaner, self).__init__(input_dir, output_dir, u"’“”\r\n'-/.,", punctuation + digits.replace('#', ''))
+
     def cleaned_text(self, text):
         urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
         for url in urls:
             text = text.replace(url, '')
         text = text.replace('&quot;', '')
-        text = text.replace('&lt;','')
-        text = text.replace('&gt;','')
-        text = text.replace('&amp;','')
-        return super(TweetCleaner,self).cleaned_text(text)
-    
-    
+        text = text.replace('&lt;', '')
+        text = text.replace('&gt;', '')
+        text = text.replace('&amp;', '')
+        text = re.sub('\\brt\\b', '', text)
+        return super(TweetCleaner, self).cleaned_text(text)
+
+
 def clean_tweets(src_dir, dest_dir):
     c = TweetCleaner(src_dir, dest_dir)
     c.clean()

dodo.py
@@ -50,7 +50,7 @@ from build.common.make_token_file import make_token_file
 import import_scripts.dataset_import
 import import_scripts.analysis_import
 from build.common.db_cleanup import remove_dataset, remove_analysis
-from topic_modeling.visualize.models import Analysis, Dataset, TopicMetric, PairwiseTopicMetric, DocumentMetric, PairwiseDocumentMetric,\
+from topic_modeling.visualize.models import Analysis, Dataset, TopicMetric, PairwiseTopicMetric, DocumentMetric, PairwiseDocumentMetric, \
     TopicNameScheme
 from helper_scripts.name_schemes.tf_itf import TfitfTopicNamer
 from helper_scripts.name_schemes.top_n import TopNTopicNamer
@@ -59,7 +59,8 @@ from helper_scripts.name_schemes.top_n import TopNTopicNamer
 # TODO(matt): Pretty hackish, but it's a starting place.  This should be
 # cleaned up when we have time.
 
-build = "state_of_the_union"
+build = "twitter"
+#build = "state_of_the_union"
 #build = "kcna/kcna"
 #build = "congressional_record"
 
@@ -68,9 +69,9 @@ if __name__ == "__main__":
     from doit.doit_cmd import cmd_main
     path = os.path.abspath(sys.argv[0])
     print 'path: ' + path
-    
-    db_name = ".{0}.db".format(build.replace('/','_'))
-    
+
+    db_name = ".{0}.db".format(build.replace('/', '_'))
+
     args = ['-f', path] + ['--db', db_name] + sys.argv[1:]
     sys.exit(cmd_main(args))
 
@@ -145,7 +146,7 @@ if 'pairwise_topic_metrics' not in locals():
 if 'document_metrics' not in locals():
     document_metrics = ['token count', 'type count', 'topic entropy']
 if 'pairwise_document_metrics' not in locals():
-    pairwise_document_metrics = ['word correlation','topic correlation']
+    pairwise_document_metrics = ['word correlation', 'topic correlation']
 if 'name_schemes' not in locals():
     name_schemes = [
                TopNTopicNamer(dataset_name, analysis_name, 2),
@@ -158,7 +159,7 @@ if 'java_base' not in locals():
 if 'java_bin' not in locals():
     java_bin = java_base + "/bin"
 if 'graph_builder_class' not in locals():
-    graph_builder_class="edu.byu.nlp.topicvis.TopicMapGraphBuilder"
+    graph_builder_class = "edu.byu.nlp.topicvis.TopicMapGraphBuilder"
 if 'graphs_min_value' not in locals():
     graphs_min_value = 1
 if 'graphs_base_url' not in locals():
@@ -196,7 +197,7 @@ if 'task_mallet_input' not in locals():
         task = dict()
         task['targets'] = [mallet_input]
         task['actions'] = [(make_token_file, [files_dir, mallet_input])]
-        task['clean']   = ["rm -f "+mallet_input]
+        task['clean'] = ["rm -f " + mallet_input]
         if 'task_extract_data' in locals():
             task['task_dep'] = ['extract_data']
         task['uptodate'] = [os.path.exists(mallet_input)]
@@ -209,10 +210,10 @@ if 'task_mallet_imported_data' not in locals():
         task['targets'] = [mallet_imported_data]
         cmd = '{0} import-file --input {1} --output {2} --keep-sequence --set-source-by-name --remove-stopwords'.format(mallet, mallet_input, mallet_imported_data)
         if token_regex is not None:
-            cmd += " --token-regex "+token_regex
+            cmd += " --token-regex " + token_regex
         task['actions'] = [cmd]
         task['file_dep'] = [mallet_input]
-        task['clean'] = ["rm -f "+mallet_imported_data]
+        task['clean'] = ["rm -f " + mallet_imported_data]
         return task
 
 if 'task_mallet_output_gz' not in locals():
@@ -222,17 +223,17 @@ if 'task_mallet_output_gz' not in locals():
         task['actions'] = ['{0} train-topics --input {1} --optimize-interval {2} --num-iterations {3} --num-topics {4} --output-state {5} --output-doc-topics {6}'
                    .format(mallet, mallet_imported_data, mallet_optimize_interval, mallet_num_iterations, num_topics, mallet_output_gz, mallet_doctopics_output)]
         task['file_dep'] = [mallet_imported_data]
-        task['clean'] = ["rm -f "+mallet_output_gz,
-                 "rm -f "+mallet_doctopics_output]
+        task['clean'] = ["rm -f " + mallet_output_gz,
+                 "rm -f " + mallet_doctopics_output]
         return task
 
 if 'task_mallet_output' not in locals():
     def task_mallet_output():
         task = dict()
         task['targets'] = [mallet_output]
-        task['actions'] = ["zcat {0} > {1}".format(mallet_output_gz,mallet_output)]
+        task['actions'] = ["zcat {0} > {1}".format(mallet_output_gz, mallet_output)]
         task['file_dep'] = [mallet_output_gz]
-        task['clean'] = ["rm -f "+mallet_output]
+        task['clean'] = ["rm -f " + mallet_output]
         return task
 
 if 'task_mallet' not in locals():
@@ -264,7 +265,7 @@ if 'task_analysis_import' not in locals():
                 d = Dataset.objects.get(name=dataset_name)
                 Analysis.objects.get(dataset=d, name=analysis_name)
                 return True
-            except (Dataset.DoesNotExist,Analysis.DoesNotExist):
+            except (Dataset.DoesNotExist, Analysis.DoesNotExist):
                 return False
         task = dict()
         task['actions'] = [(import_scripts.analysis_import.main, [dataset_name, attributes_file, analysis_name, analysis_description, mallet_output, mallet_input, files_dir, token_regex])]
@@ -291,20 +292,20 @@ if 'task_name_schemes' not in locals():
             ns.name_all_topics()
         def clean_names(ns):
             ns.unname_all_topics()
-        
+
         print "Available topic name schemes: " + u', '.join([ns.scheme_name() for ns in name_schemes])
         for ns in name_schemes:
             task = dict()
             task['name'] = ns.scheme_name()
-            task['actions'] = [(generate_names,[ns])]
+            task['actions'] = [(generate_names, [ns])]
             task['task_dep'] = ['analysis_import']
-            task['clean'] = [(clean_names,[ns])]
+            task['clean'] = [(clean_names, [ns])]
             yield task
 
 if 'task_topic_metrics' not in locals():
     def task_topic_metrics():
         from metric_scripts.topics import metrics
-        
+
         def metric_in_database(topic_metric):
             try:
                 dataset = Dataset.objects.get(name=dataset_name)
@@ -313,9 +314,9 @@ if 'task_topic_metrics' not in locals():
                 for name in names:
                     TopicMetric.objects.get(analysis=analysis, name=name)
                 return True
-            except (Dataset.DoesNotExist,Analysis.DoesNotExist,TopicMetric.DoesNotExist):
+            except (Dataset.DoesNotExist, Analysis.DoesNotExist, TopicMetric.DoesNotExist):
                 return False
-        
+
         def import_metric(topic_metric):
             start_time = datetime.now()
             print 'Adding %s...' % topic_metric,
@@ -331,7 +332,7 @@ if 'task_topic_metrics' not in locals():
             except RuntimeError as e:
                 print "\nThere was an error importing the specified metric:", e
                 sys.stdout.flush()
-        
+
         def clean_metric(topic_metric):
             print "Removing topic metric: " + topic_metric
             dataset = Dataset.objects.get(name=dataset_name)
@@ -339,21 +340,21 @@ if 'task_topic_metrics' not in locals():
             names = metrics[topic_metric].metric_names_generated(dataset_name, analysis_name)
             for topic_metric_name in names:
                 TopicMetric.objects.get(analysis=analysis, name=topic_metric_name).delete()
-        
+
         print "Available topic metrics: " + u', '.join(topic_metrics)
         for topic_metric in topic_metrics:
             task = dict()
-            task['name'] = topic_metric.replace(' ','_')
+            task['name'] = topic_metric.replace(' ', '_')
             task['actions'] = [(import_metric, [topic_metric])]
             task['clean'] = ["ls", (clean_metric, [topic_metric])]
             task['task_dep'] = ['analysis_import']
             task['uptodate'] = [metric_in_database(topic_metric)]
             yield task
-    
+
 if 'task_pairwise_topic_metrics' not in locals():
     def task_pairwise_topic_metrics():
         from metric_scripts.topics import pairwise_metrics
-        
+
         def metric_in_database(metric):
             try:
                 dataset = Dataset.objects.get(name=dataset_name)
@@ -362,9 +363,9 @@ if 'task_pairwise_topic_metrics' not in locals():
                 for name in names:
                     PairwiseTopicMetric.objects.get(analysis=analysis, name=name)
                 return True
-            except (Dataset.DoesNotExist,Analysis.DoesNotExist,PairwiseTopicMetric.DoesNotExist):
+            except (Dataset.DoesNotExist, Analysis.DoesNotExist, PairwiseTopicMetric.DoesNotExist):
                 return False
-        
+
         def import_metric(metric):
             start_time = datetime.now()
             print 'Adding %s...' % metric,
@@ -380,7 +381,7 @@ if 'task_pairwise_topic_metrics' not in locals():
             except RuntimeError as e:
                 print "\nThere was an error importing the specified metric:", e
                 sys.stdout.flush()
-        
+
         def clean_metric(metric):
             print "Removing pairwise topic metric: " + metric
             dataset = Dataset.objects.get(name=dataset_name)
@@ -388,11 +389,11 @@ if 'task_pairwise_topic_metrics' not in locals():
             names = pairwise_metrics[metric].metric_names_generated(dataset_name, analysis_name)
             for metric_name in names:
                 PairwiseTopicMetric.objects.get(analysis=analysis, name=metric_name).delete()
-        
+
         print "Available pairwise topic metrics: " + u', '.join(pairwise_metrics)
         for pairwise_topic_metric in pairwise_metrics:
             task = dict()
-            task['name'] = pairwise_topic_metric.replace(' ','_')
+            task['name'] = pairwise_topic_metric.replace(' ', '_')
             task['actions'] = [(import_metric, [pairwise_topic_metric])]
             task['clean'] = [(clean_metric, [pairwise_topic_metric])]
             task['task_dep'] = ['analysis_import']
@@ -402,7 +403,7 @@ if 'task_pairwise_topic_metrics' not in locals():
 if 'task_document_metrics' not in locals():
     def task_document_metrics():
         from metric_scripts.documents import metrics
-        
+
         def metric_in_database(metric):
             try:
                 dataset = Dataset.objects.get(name=dataset_name)
@@ -411,9 +412,9 @@ if 'task_document_metrics' not in locals():
                 for name in names:
                     DocumentMetric.objects.get(analysis=analysis, name=name)
                 return True
-            except (Dataset.DoesNotExist,Analysis.DoesNotExist,DocumentMetric.DoesNotExist):
+            except (Dataset.DoesNotExist, Analysis.DoesNotExist, DocumentMetric.DoesNotExist):
                 return False
-        
+
         def import_metric(metric):
             start_time = datetime.now()
             print 'Adding %s...' % metric,
@@ -429,7 +430,7 @@ if 'task_document_metrics' not in locals():
             except RuntimeError as e:
                 print "\nThere was an error importing the specified metric:", e
                 sys.stdout.flush()
-        
+
         def clean_metric(metric):
             print "Removing document metric: " + metric
             dataset = Dataset.objects.get(name=dataset_name)
@@ -437,11 +438,11 @@ if 'task_document_metrics' not in locals():
             names = metrics[metric].metric_names_generated(dataset_name, analysis_name)
             for metric_name in names:
                 metric = DocumentMetric.objects.get(analysis=analysis, name=metric_name).delete()
-        
+
         print "Available document metrics: " + u', '.join(metrics)
         for metric in metrics:
             task = dict()
-            task['name'] = metric.replace(' ','_')
+            task['name'] = metric.replace(' ', '_')
             task['actions'] = [(import_metric, [metric])]
             task['clean'] = ["ls", (clean_metric, [metric])]
             task['task_dep'] = ['analysis_import']
@@ -451,7 +452,7 @@ if 'task_document_metrics' not in locals():
 if 'task_pairwise_document_metrics' not in locals():
     def task_pairwise_document_metrics():
         from metric_scripts.documents import pairwise_metrics
-        
+
         def metric_in_database(metric):
             try:
                 dataset = Dataset.objects.get(name=dataset_name)
@@ -460,9 +461,9 @@ if 'task_pairwise_document_metrics' not in locals():
                 for name in names:
                     PairwiseDocumentMetric.objects.get(analysis=analysis, name=name)
                 return True
-            except (Dataset.DoesNotExist,Analysis.DoesNotExist,PairwiseDocumentMetric.DoesNotExist):
+            except (Dataset.DoesNotExist, Analysis.DoesNotExist, PairwiseDocumentMetric.DoesNotExist):
                 return False
-        
+
         def import_metric(metric):
             start_time = datetime.now()
             print 'Adding %s...' % metric,
@@ -478,7 +479,7 @@ if 'task_pairwise_document_metrics' not in locals():
             except RuntimeError as e:
                 print "\nThere was an error importing the specified metric:", e
                 sys.stdout.flush()
-        
+
         def clean_metric(metric):
             print "Removing pairwise document metric: " + metric
             dataset = Dataset.objects.get(name=dataset_name)
@@ -487,11 +488,11 @@ if 'task_pairwise_document_metrics' not in locals():
             if isinstance(names, basestring): names = [names]
             for metric_name in names:
                 PairwiseDocumentMetric.objects.get(analysis=analysis, name=metric_name).delete()
-        
+
         print "Available pairwise document metrics: " + u', '.join(pairwise_metrics)
         for metric in pairwise_metrics:
             task = dict()
-            task['name'] = metric.replace(' ','_')
+            task['name'] = metric.replace(' ', '_')
             task['actions'] = [(import_metric, [metric])]
             task['clean'] = [(clean_metric, [metric])]
             task['task_dep'] = ['analysis_import']
@@ -499,7 +500,7 @@ if 'task_pairwise_document_metrics' not in locals():
 
 if 'task_metrics' not in locals():
     def task_metrics():
-        return {'actions':None, 'task_dep': ['analysis_import', 'topic_metrics','pairwise_topic_metrics','document_metrics','pairwise_document_metrics']}
+        return {'actions':None, 'task_dep': ['analysis_import', 'topic_metrics', 'pairwise_topic_metrics', 'document_metrics', 'pairwise_document_metrics']}
 
 def task_hash_java():
     return {'actions': [(directory_recursive_hash, [java_base])]}
@@ -508,7 +509,7 @@ if 'task_java' not in locals():
     def task_java():
         actions = ["cd {0} && ant -lib lib".format(java_base)]
         result_deps = ['hash_java']
-        clean = ['rm -rf '+java_bin]
+        clean = ['rm -rf ' + java_bin]
         return {'actions':actions, 'result_dep':result_deps, 'clean':clean}
 
 if 'task_graphs' not in locals():
@@ -517,16 +518,16 @@ if 'task_graphs' not in locals():
             graphs_img_dir = "{0}/topic_maps/{1}/{2}".format(dataset_dir, analysis_name, ns.scheme_name())
             graphs_unlinked_img_dir = graphs_img_dir + "-unlinked"
             gexf_file_name = "{0}/full_graph.gexf".format(graphs_img_dir)
-            
+
             actions = [
                 'java -cp {0}:{1}/lib/gephi-toolkit.jar:{1}/lib/statnlp-rev562.jar:{1}/lib/sqlitejdbc-v056.jar {2} {3} {4} {5} {6} {7} "{8}" {9} {10} {11} {12}'
-                .format(java_bin,java_base,graph_builder_class,graphs_min_value,graphs_base_url,dataset_name,analysis_name,ns.scheme_name(),graphs_pairwise_metric, yamba_file, graphs_unlinked_img_dir, graphs_img_dir, gexf_file_name)
+                .format(java_bin, java_base, graph_builder_class, graphs_min_value, graphs_base_url, dataset_name, analysis_name, ns.scheme_name(), graphs_pairwise_metric, yamba_file, graphs_unlinked_img_dir, graphs_img_dir, gexf_file_name)
             ]
             task_deps = ['pairwise_topic_metrics', 'name_schemes', 'java']
             result_deps = ['hash_java']
-            clean =  [
-                'rm -rf '+graphs_unlinked_img_dir,
-                'rm -rf '+graphs_img_dir
+            clean = [
+                'rm -rf ' + graphs_unlinked_img_dir,
+                'rm -rf ' + graphs_img_dir
             ]
             yield {'name':ns.scheme_name(), 'actions':actions, 'task_dep':task_deps, 'result_dep':result_deps, 'clean':clean}
 #

topic_modeling/media/attribute_scripts/attribute_sidebar.js
@@ -42,7 +42,6 @@ function redraw_values(values_list, page, num_pages) {
     
     $("#attributes_list").html(new_html);
 }
-
 function get_attribute_page(page) {
     $.getJSON('/feeds/attribute-page/datasets/'
             + $.fn.attr_vars.dataset

topic_modeling/media/topic_scripts/sidebar.js
@@ -37,7 +37,18 @@ function redraw_topics(topics_list, page, num_pages) {
 		new_html += '/' + topics_list[i].number;
 		new_html += $.fn.topic_vars.post_link;
 		new_html += '">';
-		new_html += topics_list[i].name+'</a></li>';
+		new_html += topics_list[i].name;
+		      if (topics_list[i].topicgroup) {
+          new_html += ' - GROUP</a></li>';
+          for(var j = 0; j < topics_list[i].topicgroup.length; j++) {
+              new_html += '<li>';
+              new_html += topics_list[i].topicgroup[j];
+              new_html += '</li>';
+          }
+        }
+        else {
+          new_html += '</a></li>';
+        }
 	}
 	new_html += '</ul>';
 	

topic_modeling/templates/base.html
@@ -50,7 +50,7 @@ Provo, UT 84602, (801) 422-9339 or 422-3821, e-mail copyright@byu.edu.
 						var url = "{{ item.url }}";
 					{% endif %}
 				{% endfor %}
-				var link = "/feeds/add_favorite?url="+encodeURIComponent(url);
+				var link = "/feeds/add_favorite/tab/{{tab}}?url="+encodeURIComponent(url);
 				$.get(link);
 			}
 		</script>
@@ -79,7 +79,7 @@ Provo, UT 84602, (801) 422-9339 or 422-3821, e-mail copyright@byu.edu.
 								<a href="/datasets/{{ dataset }}/analyses/{{ analysis }}/attributes/">Attributes</a>
 							</span>
 							<span id="favorites_tab_header" class="tab_header {% ifequal highlight "favorite_tab" %}fg{% else %}bg{% endifequal %}">
-								<a href="/favorite/datasets/{{ dataset }}/analyses/{{ analysis }}">Favorites</a>
+								<a href="/favorite/datasets/{{ dataset }}/analyses/{{ analysis }}/id/{{ favorite.id }}">Favorites</a>
 							</span>
 						</div>
 						<div id="tab_row_2" class="tab_row">

commit 04f781f7175573086373f8733659da222d5787f2

Commit 295b454bab broke topic sorting.  This fixes it.

topic_modeling/visualize/topics/ajax.py
@@ -215,11 +215,13 @@ def update_topic_word_filter(request, dataset, analysis, topic, number, word):
     request.session.modified = True
     return filtered_topics_response(request, dataset, analysis)
 
+
 class AjaxTopic(object):
     def __init__(self, topic, topic_name):
-        self.name = topic_name
+        self.name = str(topic.number) + ": " + topic_name
         self.number = topic.number
         try:
+            # TODO(matt): This looks like it gets the wrong name
             self.topicgroup = [topic.name for topic
                                in topic.topicgroup.subtopics]
         except:

topic_modeling/visualize/topics/common.py
@@ -34,11 +34,10 @@ from topic_modeling.visualize.models import TopicNameScheme
 
 def get_topic_name(topic, name_scheme_id):
     ns = TopicNameScheme.objects.get(id=name_scheme_id)
-    name = str(topic.number) + ": "
     try:
-        name += TopicName.objects.get(topic=topic, name_scheme=ns).name
+        name = TopicName.objects.get(topic=topic, name_scheme=ns).name
     except:
-        name += topic.name
+        name = topic.name
     return name
 
 def sort_topics(topics, sort_by, session):