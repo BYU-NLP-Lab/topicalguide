219b5cf3e7db09f8579b3fb97af47702a43a8cae
Josh Hansen <hansen.joshuaa@gmail.com>
2011-05-04 12:43:06 -0600

    Fix bug in which the manage.py script wasn't finding the anyjson module

--- import_scripts/dataset_import.py
+++ import_scripts/dataset_import.py
@@ -47,7 +47,7 @@ from collections import defaultdict
 from datetime import datetime
 from optparse import OptionParser
 
-import anyjson
+import topic_modeling.anyjson as anyjson
 
 # A couple of global variables just to make life easier, so I don't have to
 # pass them around so much - they're created once and then never change
