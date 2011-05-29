0e4f416d3d0df704854dafabb866810359bb0c49
Josh Hansen <hansen.joshuaa@gmail.com>
2011-05-28 21:48:02 -0600

    Add a new centroid finding method that does an entire topic in one pass

--- metric_scripts/topics/centroid.py
+++ metric_scripts/topics/centroid.py
@@ -4,11 +4,14 @@ Created on May 10, 2011
 @author: Josh Hansen
 '''
 
-from helper_scripts.pmidb import PmiDb
-from os import environ
-from topic_modeling.visualize.models import Analysis
-from math import log
+
 import sys
+import random
+from os import environ
+from math import log
+
+from helper_scripts.pmidb import PmiDb
+from topic_modeling.visualize.models import Analysis
 
 environ['DJANGO_SETTINGS_MODULE'] = 'topic_modeling.settings'
 
@@ -77,7 +80,7 @@ class CentroidFinder2:
     def __init__(self, countsfile):
         self.db = PmiDb(countsfile, isolation_level="DEFERRED")
     
-    def centroids(self, topic, n=20, min_word_count=1, min_cocount=3):
+    def centroids(self, topic, n=20, min_word_count=1.0, min_cocount=3.0):
         topic_words = topic.topicword_set.select_related().order_by('-count')
         weighted_sums = dict()
         
@@ -88,11 +91,12 @@ class CentroidFinder2:
             type = tw.word.type
             weight = float(tw.count)
             print 'word "{0}", weight {1}'.format(type, weight)
-            print 'Relevant pairs: {0}'.format(self.db.word_pair_count(type))
+            print 'Relevant pairs: {0}'.format(self.db.word_pair_count(type,min_count=min_cocount))
             
             skipped_words = 0
             skipped_cocounts = 0
-            for i,(word1,word2,cocount) in enumerate(self.db.word_pairs(type)):
+            for i,(word1,word2,cocount) in enumerate(self.db.word_pairs(type,min_count=min_cocount)):
+                cocount = float(cocount)
                 if word1 != word2:
                     if cocount==0:
                         sys.stderr.write('.')
@@ -100,24 +104,24 @@ class CentroidFinder2:
                     elif cocount < min_cocount:
                         skipped_cocounts += 1
                     else:
-                        p_joint = float(cocount) / total_cocounts
+                        p_joint = cocount / total_cocounts
                         word = word1 if type == word2 else word2
                         if i % 1000 == 0:
                             print word,
                             sys.stdout.flush()
                             if i % 20000 == 0: print
                         
-                        c_word1 = self.db.count(word1)
+                        c_word1 = float(self.db.count(word1))
                         if c_word1 < min_word_count:
                             skipped_words += 1
                         else:
-                            p_word1 = float(c_word1) / total_counts
-                            c_word2 = self.db.count(word2)
+                            p_word1 = c_word1 / total_counts
+                            c_word2 = float(self.db.count(word2))
                             if c_word2 < min_word_count:
                                 skipped_words += 1
                             else:
                                 #total_qualifying_words += 1.0
-                                p_word2 = float(c_word2) / total_counts
+                                p_word2 = c_word2 / total_counts
                                 
                                 pmi = log(p_joint) - log(p_word1) - log(p_word2)
                                 weighted_pmi = weight * pmi
@@ -142,11 +146,94 @@ class CentroidFinder2:
         f.close()
         print
 
+class CentroidFinder3:
+    def __init__(self, countsfile):
+        self.db = PmiDb(countsfile, isolation_level="DEFERRED")
+    
+    def centroids(self, topic, n=20, min_word_count=1.0, min_cocount=3.0):
+        total_counts = self.db.total_counts()
+        total_cocounts = self.db.total_cocounts()
+        
+        topic_words = topic.topicword_set.select_related().order_by('-count')
+        topic_word_weights = dict()
+        for tw in topic_words:
+            topic_word_weights[tw.word.type] = tw.count
+        topic_word_types = topic_word_weights.keys()
+        
+        weighted_sums = dict()
+        def report_status():
+            print 'Iteration '+str(i)
+            best = sorted(weighted_sums.items(), key=lambda x:x[1], reverse=True)
+            print str(best[0:n])
+        
+        def count_pair(word1, word2, word1count, word2count, pmi):
+            def increment(topic_word, other_word, topic_word_weight):
+                weighted_pmi = topic_word_weight * pmi
+                
+                try:
+                    previous_sum = weighted_sums[other_word]
+                except KeyError:
+                    previous_sum = 0.0
+                weighted_sums[other_word] = previous_sum + weighted_pmi
+            
+            if word1 in topic_word_types:
+                increment(word1, word2, word1count)
+            if word2 in topic_word_types:
+                increment(word2, word1, word2count)
+        
+        print 'Relevant pairs: {0}'.format(self.db.word_pair_count(topic_word_types,min_count=min_cocount))
+            
+        skipped_words = 0
+        skipped_cocounts = 0
+        for i,(word1,word2,cocount) in enumerate(self.db.word_pairs(topic_word_types,min_count=min_cocount)):
+            cocount = float(cocount)
+            if word1 != word2:
+                if cocount==0:
+                    sys.stderr.write('.')
+                    skipped_cocounts += 1
+                elif cocount < min_cocount:
+                    skipped_cocounts += 1
+                else:
+                    p_joint = cocount / total_cocounts
+                    if i % 1000 == 0:
+                        print '({0},{1})'.format(word1,word2)
+                        sys.stdout.flush()
+                        if i % 20000 == 0: print
+                    c_word1 = float(self.db.count(word1))
+                    if c_word1 < min_word_count:
+                        skipped_words += 1
+                    else:
+                        p_word1 = c_word1 / total_counts
+                        c_word2 = float(self.db.count(word2))
+                        if c_word2 < min_word_count:
+                            skipped_words += 1
+                        else:
+                            p_word2 = c_word2 / total_counts
+                            pmi = log(p_joint) - log(p_word1) - log(p_word2)
+                            count_pair(word1, word2, c_word1, c_word2, pmi)
+        print
+        print 'Pairs skipped for lack of word counts: ' + str(skipped_words)
+        print 'Pairs skipped for lack of cocounts: ' + str(skipped_cocounts)
+        best = sorted(weighted_sums.items(), key=lambda x:x[1], reverse=True)
+        print str(best[0:n])
+        print
+        
+        f = open(environ['HOME'] + '/Projects/topicalguide/output/centroids/' + topic.dataset.name + '_' + str(topic.number) + '.txt', 'w')
+        for word,count in best.items():
+            f.write(word)
+            f.write(',')
+            f.write(str(count))
+            f.write('\n')
+        f.close()
+        print
+
 if __name__ == '__main__':
-    cf = CentroidFinder2(environ['HOME']+'/Data/wikipedia.org/wikipedia_counts3.sqlite3')
+    cf = CentroidFinder3(environ['HOME']+'/Data/wikipedia.org/wikipedia_counts4.sqlite3')
     
     a = Analysis.objects.get(name='lda100topics', dataset__name='state_of_the_union')
-    for topic in a.topic_set.all().order_by('number'):
+    topics = [x for x in a.topic_set.all()]#.order_by('number')
+    random.shuffle(topics)
+    for topic in topics:
         print 'Topic ' + str(topic.number)
         for tw in sorted(topic.topicword_set.all(),key=lambda x:x.count,reverse=True)[0:20]:
             print "\t"+tw.word.type + ": " + str(tw.count)
