b8da3ce7f4fba7d89b46ca3f37d707cb370ebfde
Josh Hansen <hansen.joshuaa@gmail.com>
2011-06-01 13:31:33 -0600

    Clean up centroid code, fix a problem with a sql query

--- metric_scripts/topics/centroid.py
+++ metric_scripts/topics/centroid.py
@@ -25,13 +25,20 @@ class AbstractCentroidFinder(object):
             s += "\t"+tw.word.type + ": " + str(tw.count)
         return s
 
+    def print_status(self, weighted_sums):
+        self._print_status(self._best(weighted_sums))
+    
+    def _best(self, weighted_sums):
+        return sorted(weighted_sums.items(), key=lambda x:x[1], reverse=True)
+    
+    def _print_status(self, best):
+        print str(best[0:self.n])
 
     def save(self, topic, weighted_sums):
-        
-        best = sorted(weighted_sums.items(), key=lambda x:x[1], reverse=True)
-        print str(best[0:self.n])
+        best = self._best(weighted_sums)
+        self._print_status(best)
         print
-
+        
         f = open(environ['HOME'] + '/Projects/topicalguide/output/centroids/{0}_{1}_{2}.txt'.format(topic.analysis.dataset.name,topic.analysis.name,topic.number), 'w')
         f.write(self.topic_summary(topic))
         f.write('\n')
@@ -42,70 +49,8 @@ class AbstractCentroidFinder(object):
             f.write('\n')
         f.close()
 
-
-class BestValues:
-    def __init__(self, n, type='max', key=lambda x:x):
-        self.n = n
-        self.type = type
-        self.key = key
-        self.values = []
-    
-    def _better_than_some(self, value):
-        val_key = self.key(value)
-        if self.type is 'max':
-            for incumbent in reversed(self.values):
-                if val_key > self.key(incumbent): return True
-            return False
-        elif self.type is 'min':
-            for incumbent in self.values:
-                if val_key < self.key(incumbent): return True
-            return False
-        else: raise Exception("Unknown optimization type")
-    
-    def add(self, value):
-        still_room = len(self.values) < self.n
-        if still_room or self._better_than_some(value):
-            self.values.append(value)
-            self.values.sort(reverse=True)
-            if not still_room:
-                self.values.pop()
-
-class CentroidFinder:
-    def __init__(self, countsfile):
-        self.db = PmiDb(countsfile, isolation_level="DEFERRED")
-
-    def centroids(self, topic, n=10):
-        topic_words = topic.topicword_set.select_related()
-        i = 0
-        best = BestValues(n, key=lambda x:x[1])
-#        max = None
-#        argmax = None
-        for word in self.db.words():
-            i += 1
-            if i % 10 == 0:
-                print word,
-                if i % 100 == 0: print
-            weighted_sum = self._pmi_weighted_sum(topic_words, word)
-            best.add((word,weighted_sum))
-#            if max is None or weighted_sum > max:
-#                max = weighted_sum
-#                argmax = word
-#        return argmax
-        return best.values
-    
-    #Note that this is not normalized because we will take the argmax
-    def _pmi_weighted_sum(self, topic_words, word1):
-        sum = 0.0
-        for tw in topic_words:
-            word2 = tw.word.type
-            p_word2 = float(tw.count)
-            pmi = self.db.pmi(word1, word2)
-            if pmi is not None:
-                sum += p_word2*pmi
-        return sum
-
-class CentroidFinder2(AbstractCentroidFinder):
-    def centroids(self, topic, min_word_count=1.0, min_cocount=3.0):
+class CentroidFinder(AbstractCentroidFinder):
+    def centroids(self, topic, min_word_count=1.0, min_cocount=3):
         topic_words = topic.topicword_set.select_related().order_by('-count')
         weighted_sums = dict()
         
@@ -116,75 +61,68 @@ class CentroidFinder2(AbstractCentroidFinder):
             type = tw.word.type
             weight = float(tw.count)
             print 'word "{0}", weight {1}'.format(type, weight)
-            print 'Relevant pairs: {0}'.format(self.db.word_pair_count(type,min_count=min_cocount))
+#            print 'Relevant pairs: {0}'.format(self.db.word_pair_count(type,min_count=min_cocount))
             
             skipped_words = 0
             skipped_cocounts = 0
-            for i,(word1,word2,cocount) in enumerate(self.db.word_pairs(type,min_count=min_cocount)):
+            for i,(word1,word2,cocount) in enumerate(self.db.word_pairs(type,min_count=min_cocount,notequal=True)):
                 cocount = float(cocount)
-                if word1 != word2:
-                    if cocount < min_cocount:
-                        skipped_cocounts += 1
+                if cocount < min_cocount:
+                    skipped_cocounts += 1
+                else:
+                    p_joint = cocount / total_cocounts
+                    word = word1 if type == word2 else word2
+                    if i % 1000 == 0:
+                        print word,
+                        sys.stdout.flush()
+                        if i % 5000 == 0: print i,
+                    
+                    c_word1 = float(self.db.count(word1))
+                    if c_word1 < min_word_count:
+                        skipped_words += 1
                     else:
-                        p_joint = cocount / total_cocounts
-                        word = word1 if type == word2 else word2
-                        if i % 1000 == 0:
-                            print word,
-                            sys.stdout.flush()
-                            if i % 20000 == 0: print
-                        
-                        c_word1 = float(self.db.count(word1))
-                        if c_word1 < min_word_count:
+                        p_word1 = c_word1 / total_counts
+                        c_word2 = float(self.db.count(word2))
+                        if c_word2 < min_word_count:
                             skipped_words += 1
                         else:
-                            p_word1 = c_word1 / total_counts
-                            c_word2 = float(self.db.count(word2))
-                            if c_word2 < min_word_count:
-                                skipped_words += 1
-                            else:
-                                p_word2 = c_word2 / total_counts
-                                
-                                pmi = log(p_joint) - log(p_word1) - log(p_word2)
-                                weighted_pmi = weight * pmi
-                                
-                                try:
-                                    previous_sum = weighted_sums[word]
-                                except KeyError:
-                                    previous_sum = 0.0
-                                weighted_sums[word] = previous_sum + weighted_pmi
+                            p_word2 = c_word2 / total_counts
+                            
+                            pmi = log(p_joint) - log(p_word1) - log(p_word2)
+                            weighted_pmi = weight * pmi
+                            
+                            try:
+                                previous_sum = weighted_sums[word]
+                            except KeyError:
+                                previous_sum = 0.0
+                            weighted_sums[word] = previous_sum + weighted_pmi
             print
             print 'Pairs skipped for lack of word counts: ' + str(skipped_words)
             print 'Pairs skipped for lack of cocounts: ' + str(skipped_cocounts)
-            self.save(topic, weighted_sums)
+            self.print_status(weighted_sums)
+            print
+        self.save(topic, weighted_sums)
         print
 
-class CentroidFinder3(AbstractCentroidFinder):
+class UnifiedPassCentroidFinder(AbstractCentroidFinder):
     def centroids(self, topic, min_word_count=1.0, min_cocount=3.0):
         total_counts = self.db.total_counts()
         total_cocounts = self.db.total_cocounts()
         
         topic_words = topic.topicword_set.select_related().order_by('-count')
-        topic_word_weights = dict()
-        for tw in topic_words:
-            topic_word_weights[tw.word.type] = tw.count
-        topic_word_types = topic_word_weights.keys()
+        topic_word_types = [tw.word.type for tw in topic_words]
         
         weighted_sums = dict()
-        def report_status():
-            print 'Iteration '+str(i)
-            best = sorted(weighted_sums.items(), key=lambda x:x[1], reverse=True)
-            print str(best[0:n])
+        def increment(topic_word, other_word, topic_word_weight):
+            weighted_pmi = topic_word_weight * pmi
+            
+            try:
+                previous_sum = weighted_sums[other_word]
+            except KeyError:
+                previous_sum = 0.0
+            weighted_sums[other_word] = previous_sum + weighted_pmi
         
         def count_pair(word1, word2, word1count, word2count, pmi):
-            def increment(topic_word, other_word, topic_word_weight):
-                weighted_pmi = topic_word_weight * pmi
-                
-                try:
-                    previous_sum = weighted_sums[other_word]
-                except KeyError:
-                    previous_sum = 0.0
-                weighted_sums[other_word] = previous_sum + weighted_pmi
-            
             if word1 in topic_word_types:
                 increment(word1, word2, word1count)
             if word2 in topic_word_types:
@@ -224,7 +162,7 @@ class CentroidFinder3(AbstractCentroidFinder):
         print
 
 if __name__ == '__main__':
-    cf = CentroidFinder2(environ['HOME']+'/Data/wikipedia.org/wikipedia_counts4.sqlite3')
+    cf = CentroidFinder(environ['HOME']+'/Data/wikipedia.org/wikipedia_counts4.sqlite3')
     
     a = Analysis.objects.get(name='lda100topics', dataset__name='state_of_the_union')
     topics = [x for x in a.topic_set.all()]#.order_by('number')
