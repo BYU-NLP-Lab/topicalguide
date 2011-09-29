6aa74c48e3fe60f19f2e2a7f034c2411d532b404
Josh Hansen <hansen.joshuaa@gmail.com>
2011-09-29 16:43:29 -0600

    Add a primitive version of the favorites dropdown menu

--- topic_modeling/visualize/favorite.py
+++ topic_modeling/visualize/favorite.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render_to_response
-
 # The Topic Browser
 # Copyright 2010-2011 Brigham Young University
 #
@@ -22,6 +20,10 @@ from django.shortcuts import render_to_response
 # contact the Copyright Licensing Office, Brigham Young University, 3760 HBLL,
 # Provo, UT 84602, (801) 422-9339 or 422-3821, e-mail copyright@byu.edu.
 
+from django.shortcuts import render_to_response
+from topic_modeling import anyjson
+from django.http import HttpResponse, HttpResponseRedirect
+import copy
 
 def index(request, dataset, analysis, id):
     page_vars = dict()
@@ -46,3 +48,65 @@ def index(request, dataset, analysis, id):
 
     return render_to_response('favorite.html', page_vars)
 
+class Favorite:
+    def __init__(self, url, id, session, tab):
+        self.name = tab + ":" + url.split('/')[-1]
+        self.url = url
+        self.id = id
+        self.session_recall = {}
+        for key in session.keys():
+            if key.startswith(tab):
+                self.session_recall[key] = copy.deepcopy(session[key])
+        self.tab = tab
+
+    def __hash__(self):
+        return self.id
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __cmp__(self, other):
+        return self.id - other.id
+
+#AJAX Calls
+def get_favorite_page(request, number):
+    ret_val = dict()
+    favorites = request.session.get('favorite-set', set())
+    ret_val['favorites'] = [{'id' : fav.id, 'name' : fav.name}
+                            for fav in favorites]
+    ret_val['num_pages'] = 1
+    ret_val['page'] = 1
+
+    return HttpResponse(anyjson.dumps(ret_val))
+
+def add_favorite(request, tab):
+    favorites = request.session.get('favorite-set', set())
+    id = request.session.get('last-favorite-id', 0)
+    url = request.GET['url']
+    favorite = Favorite(url, id, request.session, tab)
+    favorites.add(favorite)
+    request.session['last-favorite-id'] = favorite.id + 1
+    request.session['favorite-set'] = favorites
+    return HttpResponse('Favorite Added:' + favorite.url)
+
+def remove_all_favorites(request):
+    if 'favorite-set' in request.session:
+        request.session.remove('favorite-list')
+    return HttpResponse('Favorites cleared')
+
+def remove_favorite(request, url):
+    favorites = request.session.get('favorite-set', set())
+    if url in favorites:
+        favorites.remove(url)
+    request.session['favorite-set'] = favorites
+    return HttpResponse('Url Removed:' + url)
+
+def recall_favorite(request, id):
+    favorites = request.session.get('favorite-set', set())
+    id = int(id)
+    for favorite in favorites:
+        if favorite.id == id:
+            for key in favorite.session_recall.keys():
+                request.session[key] = copy.deepcopy(favorite.session_recall[key])
+            return HttpResponseRedirect(favorite.url)
+    return HttpResponse('Unknown Favorite')
