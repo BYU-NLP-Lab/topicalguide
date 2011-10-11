7b110f2b1424a5ba042983bcd3d24cc79780fea1
Jeff Lund <jlund3@aml.cs.byu.edu>
2011-10-11 14:37:29 -0600

    Added export button to topic-attribute plots on plots tab.

--- topic_modeling/media/scripts/plots.js
+++ topic_modeling/media/scripts/plots.js
@@ -13,9 +13,11 @@ function highlight_all_topic_attribute_values(refresh) {
 
 function update_topic_attribute_plot() {
     set_loading_image();
-    var url_base = "/feeds/topic-attribute-plot/";
+    var plot_url_base = "/feeds/topic-attribute-plot/";
+    var csv_url_base = "/feeds/topic-attribute-csv/";
+    var url_path = "";
     attribute = $("select#id_attribute").val();
-    url_base += 'attributes/'+attribute+'/';
+    url_path += 'attributes/'+attribute+'/';
     
     if ($("select#id_topics").val() == null) {
         $("select#id_topics option:first").attr("selected", true);
@@ -27,36 +29,37 @@ function update_topic_attribute_plot() {
     // Now get selected values and make a list of them 
     var selected = $("select#id_values").val();
     selected = selected.sort(numerical_sort_function);
-    url_base += "values/" + selected[0];
+    url_path += "values/" + selected[0];
     for (var i = 1; i < selected.length; i++) {
-        url_base += "." + selected[i];
+        url_path += "." + selected[i];
     }
-    url_base += "/topics";
+    url_path += "/topics";
 
     // Now get selected topics and make a list of them 
     var selected = $("select#id_topics").val()
-    url_base += '/';
+    url_path += '/';
     selected = selected.sort(numerical_sort_function)
-    url_base += selected[0];
+    url_path += selected[0];
     for (var i = 1; i < selected.length; i++) {
-        url_base += "." + selected[i];
+        url_path += "." + selected[i];
     }
     var added_query = false;
     if ($('#id_by_frequency:checked').val() != null) {
-        url_base += "?frequency=true";
+        url_path += "?frequency=true";
         added_query = true;
     }
     if ($('#id_histogram:checked').val() != null) {
         if (!added_query) {
-            url_base += '?';
+            url_path += '?';
         } else {
-            url_base += '&';
+            url_path += '&';
         }
-        url_base += "histogram=true";
+        url_path += "histogram=true";
     }
     // When  the URL is ready, change the source of the
     // image to point to the correct one
-    $("img#plot_image").attr("src", url_base);
+    $("img#plot_image").attr("src", plot_url_base + url_path);
+    $("a#csv_data").attr("href", csv_url_base + url_path);
 }
 
 /* The 'analysis' argument is retained so the signature is the same as that of update_topic_metric_plot */
