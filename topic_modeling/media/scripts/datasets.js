074a874f5ad2ead70af6451ae6ccff4ee17b9ac4
Josh Hansen <joshuaaaronhansen@gmail.com>
2011-12-07 00:28:56 -0700

    Fix bug in which analysis url was broken

--- topic_modeling/media/scripts/datasets.js
+++ topic_modeling/media/scripts/datasets.js
@@ -1,24 +1,29 @@
 $(document).ready(function() {
     $("#accordion").accordion({autoHeight: false});
-//    $(".dataset-sections").accordion({collapsible: true,autoHeight: false});
+    
+    //Event handlers
     $("button.explore").button().click(explore);
     $("img.plot").click(explore);
+    $("select.analysis").select(load_plot_image);
     
-    $("select#analysis").select(function(){
-    	
-    });
-    
-    //image_preview();
-    $(".analysis-link").click(function(){
-        $("html").hide("fade", {}, 250);
-    });
     bind_favorites();
 });
 
 function explore() {
-	window.location.href = $("select#analysis > option:selected").attr("url");
+	var dataset = _dataset_node($(this));
+	window.location.href = $("select.analysis > option:selected", dataset).attr("url");
 }
 
 function load_plot_image() {
-	$("select#analysis > option:selected").attr("img_url")
+	var dataset = _dataset_node($(this));
+	var img = $("img.plot", dataset);
+	var selected = $("select.analysis > option:selected", dataset);
+	img.attr('src', selected.attr('img_url'));
+}
+
+function _dataset_node(element) {
+	while(!element.attr('dataset')) {
+		element = element.parent();
+	}
+	return element;
 }
