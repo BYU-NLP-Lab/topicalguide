980c574c1159a62ad256ad173e4f9f4c6e9e54f6
Josh Hansen <hansen.joshuaa@gmail.com>
2012-01-06 17:53:00 -0700

    Refresh KCNA build

--- build/kcna.py
+++ build/kcna.py
@@ -1,10 +1,59 @@
+# The Topical Guide
+# Copyright 2010-2011 Brigham Young University
+#
+# This file is part of the Topical Guide <http://nlp.cs.byu.edu/topic_browser>.
+#
+# The Topical Guide is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# The Topical Guide is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with the Topical Guide.  If not, see <http://www.gnu.org/licenses/>.
+#
+# If you have inquiries regarding any further use of the Topical Guide, please
+# contact the Copyright Licensing Office, Brigham Young University, 3760 HBLL,
+# Provo, UT 84602, (801) 422-9339 or 422-3821, e-mail copyright@byu.edu.
+
 #wget --mirror http://kcna.co.jp/item/2011/calendar-2011e.html
-import os
-import subprocess as sub
-import re
+
 import codecs
-import topic_modeling.anyjson as anyjson
+import os
+import re
 import sys
+import subprocess as sub
+
+from backend import c
+import topic_modeling.anyjson as anyjson
+
+c['dataset_name'] = "kcna"
+c['dataset_description'] = "News releases/propaganda from North Korea's Korean Central News Agency (KCNA)"
+c['url'] = "http://kcna.co.jp/"
+c['data_dir'] = os.environ['HOME'] + "/Data"
+c['kcna_dir'] = c['data_dir'] + "/kcna.co.jp"
+c['suppress_default_attributes_task'] = True
+
+def task_download_kcna():
+    task = dict()
+    task['actions'] = ["mkdir -p {kcna_dir} && cd {kcna_dir}  && wget --mirror -nH {url}".format(kcna_dir=c['kcna_dir'],url=c['url'])]
+    task['clean'] = ["rm -rf " + c['kcna_dir']]
+    task['uptodate'] = [os.path.exists(c['kcna_dir'])]
+    return task
+
+def task_extract_data():
+    task = dict()
+    task['targets'] = [c['files_dir'], c['metadata_filenames']['documents']]
+    task['actions'] = ["mkdir -p "+c['files_dir'],
+               (extract, [c['kcna_dir'], c['files_dir'], c['metadata_filenames']['documents']])]
+    task['clean'] = ['rm -rf '+c['files_dir'], 'rm -f '+c['metadata_filenames']['documents']]
+    task['task_dep'] = ['download_kcna']
+    task['uptodate'] = [os.path.exists(c['files_dir']) and os.path.exists(c['metadata_filenames']['documents'])]
+    return task
 
 old_date_regex = r'.+item/(?P<year>\d\d(?P<year_short>\d\d))/(?:(?P=year)|(?P=year_short))(?P<month>\d(?P<month_digit_two>\d))/news(?:(?P=month)|(?P=month_digit_two))/(?P<day>\d\d)\.htm'
 new_date_regex_strict = r'.+item/(?P<year>\d\d\d\d)/(?P=year)(?P<month>\d\d)/news(?P<day>\d\d)/(?P=year)(?P=month)(?P=day)-(?P<item>\d\d)ee?\.html'
@@ -24,13 +73,13 @@ def parse_date(path):
     else:
         #/home/jjfresh/Data/kcna.co.jp/kcna.co.jp/item/2004/200410/news10/27.htm
         regex = old_date_regex
-        dict = re.match(regex, path).groupdict()
-        dict['item'] = '01'
-        return dict
+        d = re.match(regex, path).groupdict()
+        d['item'] = '01'
+        return d
 
 def extract(src_dir, dest_dir, attributes_file):
     attributes = []
-    for dirpath, dirnames, filenames in [x for x in os.walk(src_dir) if 'news' in x[0]]:
+    for dirpath, _dirnames, filenames in [x for x in os.walk(src_dir) if 'news' in x[0]]:
         for filename in filenames:
             full_path = dirpath + '/' + filename
             date = parse_date(full_path)
@@ -66,8 +115,3 @@ def extract(src_dir, dest_dir, attributes_file):
     f = codecs.open(attributes_file, mode='w', encoding='utf-8')
     f.write(anyjson.dumps(attributes))
     f.close()
-
-
-
-if __name__ == '__main__':
-    extract('/home/jjfresh/Data/kcna.co.jp/kcna.co.jp/item', '/home/jjfresh/Projects/topicbrowser/datasets/kcna/files', '/home/jjfresh/Projects/topicbrowser/datasets/kcna/attributes.json')
