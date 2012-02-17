68a62dea04f55c2cb90b21faf5f28da21f46abef
Jeff Lund <jlund3@aml.cs.byu.edu>
2012-02-17 16:35:43 -0700

    Added licesnse to footer. Just need to update the url in views.py to point to the right thing.

--- topic_modeling/visualize/common/views.py
+++ topic_modeling/visualize/common/views.py
@@ -25,9 +25,10 @@ from datetime import timedelta
 from django.shortcuts import get_object_or_404
 from django.template.context import Context
 from django.views.generic.base import TemplateResponseMixin, View
+from django.conf import settings
 
 from topic_modeling.visualize.models import Dataset, Analysis
-from topic_modeling.visualize.favorites import dataset_favorite_entries, analysis_favorite_entries, favorite_topic_entries,\
+from topic_modeling.visualize.favorites import dataset_favorite_entries, analysis_favorite_entries, favorite_topic_entries, \
     topic_view_favorite_entries, document_view_favorite_entries
 
 '''
@@ -36,23 +37,23 @@ Like TemplateView, but better
 class RootView(TemplateResponseMixin, View):
     def get_context_data(self, request, **kwargs):
         context = Context()
-        
+
         STATIC = '/site-media'
         context['SCRIPTS'] = '/scripts'
         context['STYLES'] = '/styles'
         context['IMAGES'] = STATIC + '/images'
         context['FONTS'] = STATIC + '/fonts'
-        
+        context['license'] = "http://www.gnu.org/licenses/agpl.html"
         context['topical_guide_project_url'] = "http://nlp.cs.byu.edu/topicalguide"
         context['nlp_lab_url'] = "http://nlp.cs.byu.edu"
         context['nlp_lab_logo_url'] = context['IMAGES'] + "/byunlp-135px.png"
         context['nlp_lab_small_logo_url'] = context['IMAGES'] + "/byunlp-35px.png"
-        
+
         # Favorites Stuff
         # Do what's necessary to keep the session from ever expiring (assuming the user checks in every 100 years or so
         if request.session.get_expiry_age() < 3153600000: # If the session is expiring sometime in the next 100 years,
             request.session.set_expiry(timedelta(365000)) # then reset the expiration to 1,000 years from now
-        
+
         # Preload lists of favorites
         context['favorites'] = {
             'datasets': dataset_favorite_entries(request),
@@ -60,7 +61,7 @@ class RootView(TemplateResponseMixin, View):
             'topics': favorite_topic_entries(request) + topic_view_favorite_entries(request),
             'documents': document_view_favorite_entries(request)
         }
-        
+
         context['favids'] = {
             'datasets': [fav['fav'].dataset.id for fav in context['favorites']['datasets']],
             'analyses': [fav['fav'].analysis.id for fav in context['favorites']['analyses']],
@@ -68,9 +69,9 @@ class RootView(TemplateResponseMixin, View):
             'documents':     [fav['fav'].document.id for fav in context['favorites']['documents']]
         }
 
-        
+
         return context
-    
+
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(request, **kwargs)
         return self.render_to_response(context)
@@ -83,27 +84,27 @@ class DatasetBaseView(RootView):
             dataset = get_object_or_404(Dataset, name=kwargs['dataset'])
         except KeyError:
             dataset = context['datasets'][0]
-        
+
         context['dataset'] = dataset
         context['dataset_url'] = "/datasets/%s" % (dataset)
-        
+
         return context
 
 class AnalysisBaseView(DatasetBaseView):
     def get_context_data(self, request, **kwargs):
         context = super(AnalysisBaseView, self).get_context_data(request, **kwargs)
-        
+
         analysis = get_object_or_404(Analysis, name=kwargs['analysis'], dataset=context['dataset'])
         context['analysis'] = analysis
         context['analysis_url'] = "%s/analyses/%s" % (context['dataset_url'],
                 analysis.name)
-    
+
         context['attributes_url'] = context['analysis_url'] + "/attributes"
         context['documents_url'] = context['analysis_url'] + "/documents"
         context['plots_url'] = context['analysis_url'] + "/plots"
         context['topics_url'] = context['analysis_url'] + "/topics"
         context['words_url'] = context['analysis_url'] + "/words"
-        
+
 #        context['favorites']['topics'] = favorites.favorite_topic_entries(request, context['dataset'], analysis.name)
-        
+
         return context
