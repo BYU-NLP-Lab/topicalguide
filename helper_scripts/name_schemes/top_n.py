374c1916a692752d3af0f2419c952f24181ef417
Josh Hansen <hansen.joshuaa@gmail.com>
2012-03-20 23:27:52 -0600

    Fix bug in top n topic name scheme, and simplify and clean the code, which was an amateurish mess.

--- helper_scripts/name_schemes/top_n.py
+++ helper_scripts/name_schemes/top_n.py
@@ -21,65 +21,47 @@
 # Provo, UT 84602, (801) 422-9339 or 422-3821, e-mail copyright@byu.edu.
 
 
-from __future__ import division
-
 from django.db.models.aggregates import Count
 
 from django.db import transaction
 
-from topic_modeling.visualize.models import Analysis, Dataset
-from topic_modeling.visualize.models import TopicNameScheme,TopicName,Topic
+from topic_modeling.visualize.models import Analysis, TopicNameScheme
 
 class TopNTopicNamer:
     def __init__(self, dataset_name, analysis_name, n):
         self.dataset_name = dataset_name
         self.analysis_name = analysis_name
         self.n = n
-
-    def init(self):
-        dataset = Dataset.objects.get(name=self.dataset_name)
-        analysis = Analysis.objects.get(dataset=dataset, name=self.analysis_name)
-        name_scheme,created = TopicNameScheme.objects.get_or_create(name=self.scheme_name(),analysis=analysis)
-        return dataset,analysis,name_scheme,created
     
     def scheme_name(self):
         return "Top" + str(self.n)
     
     @transaction.commit_manually
     def name_all_topics(self):
-        _,analysis,name_scheme,created = self.init()
+        analysis = Analysis.objects.get(dataset__name=self.dataset_name, name=self.analysis_name)
+        name_scheme,created = analysis.topicnameschemes.get_or_create(name=self.scheme_name())
         
         if created:
-            topics = Topic.objects.filter(analysis=analysis)
-            for topic in topics:
-                print "topic:", topic
+            for i, topic in enumerate(analysis.topics.all()):
                 name = self.topic_name(topic)
-                print name.encode('utf-8')
-                TopicName.objects.create(topic=topic,name_scheme=name_scheme,name=name)
+                print 'topic #%i: %s' % (i, name.encode('utf-8'))
+                name_scheme.names.create(topic=topic, name=name)
             transaction.commit()
         else:
-            print "Name scheme {0} already exists for analysis {1}. Skipping.".format(name_scheme, analysis)
+            print "Name scheme %s already exists for analysis %s. Skipping." % (name_scheme, analysis)
     
     @transaction.commit_manually
     def unname_all_topics(self):
-        _,_,name_scheme,_ = self.init()
-        name_scheme.delete()
-        transaction.commit()
+        try:
+            analysis = Analysis.objects.get(dataset__name=self.dataset_name, name=self.analysis_name)
+            name_scheme = analysis.topicnameschemes.get(name=self.scheme_name())
+            name_scheme.delete()
+            transaction.commit()
+        except TopicNameScheme.DoesNotExist:
+            pass
     
     def topic_name(self,topic):
-        name = u""
-        rankings = self.ranked_topic_terms(topic)
-
-        i = 0
-        # Protect against topics with small numbers of words
-        while i < min(self.n, len(rankings)):
-            name += rankings[i].type.type
-            if i < self.n-1: name += u' '
-            i += 1
-        return name
-    
-    def ranked_topic_terms(self,topic):
-        return topic.tokens.annotate(count=Count('id')).order_by('-count')
-
-    
-# vim: et sw=4 sts=4
+        top_n_items = topic.tokens.values("type__type")\
+                      .annotate(count=Count("type__type"))\
+                      .order_by('-count')[:self.n]
+        return u' '.join(x['type__type'] for x in top_n_items)
