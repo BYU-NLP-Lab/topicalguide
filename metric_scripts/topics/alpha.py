90235f66e5e74b82b92383ab301639c3b2b8568a
Josh Hansen <hansen.joshuaa@gmail.com>
2012-03-21 00:38:12 -0600

    Clean up an obscure metric

--- metric_scripts/topics/alpha.py
+++ metric_scripts/topics/alpha.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 # The Topical Guide
 # Copyright 2010-2011 Brigham Young University
 #
@@ -25,31 +23,19 @@
 
 from __future__ import division
 
-import os, sys
-
-sys.path.append(os.curdir)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'topic_modeling.settings'
-
 from django.db import transaction
-from math import log
-from optparse import OptionParser
 
 from topic_modeling.visualize.models import Analysis, TopicMetric
 from topic_modeling.visualize.models import TopicMetricValue
 
 metric_name = 'Alpha'
 @transaction.commit_manually
-def add_metric(dataset, analysis, force_import=False, *args, **kwargs):
+def add_metric(dataset, analysis, **kwargs):
     analysis = Analysis.objects.get(dataset__name=dataset, name=analysis)
-
-    try:
-        metric = TopicMetric.objects.get(name=metric_name, analysis=analysis)
-        if not force_import:
-            raise RuntimeError('%s is already in the database for this '
-                    'analysis!' % metric_name)
-    except TopicMetric.DoesNotExist:
-        metric = TopicMetric(name=metric_name, analysis=analysis)
-        metric.save()
+    metric, created = TopicMetric.objects.get_or_create(name=metric_name, analysis=analysis)
+    if not created:
+        raise RuntimeError('%s is already in the database for this analysis!' % metric_name)
+    
     if 'state_file' not in kwargs:
         raise RuntimeError('I need a state file for this metric!')
     state_file = open(kwargs['state_file'])
@@ -59,41 +45,9 @@ def add_metric(dataset, analysis, force_import=False, *args, **kwargs):
     alphas = alpha_vector.split(': ')[1].split()
     for number, alpha in enumerate(alphas):
         topic = analysis.topics.get(number=number)
-        tmv = TopicMetricValue(topic=topic, metric=metric,
-                value=float(alpha))
-        tmv.save()
+        TopicMetricValue.objects.create(topic=topic, metric=metric, value=float(alpha))
     transaction.commit()
 
 
 def metric_names_generated(dataset, analysis):
     return [metric_name]
-
-
-if __name__ == '__main__':
-    parser = OptionParser()
-    parser.add_option('-d', '--dataset-name',
-            dest='dataset_name',
-            help='The name of the dataset for which to add this topic metric',
-            )
-    parser.add_option('-a', '--analysis-name',
-            dest='analysis_name',
-            help='The name of the analysis for which to add this topic metric',
-            )
-    parser.add_option('-s', '--state-file',
-            dest='state_file',
-            help='The state file from which I can find the alpha values',
-            )
-    parser.add_option('-f', '--force-import',
-            dest='force_import',
-            action='store_true',
-            help='Force the import of this metric even if the script thinks the'
-            ' metric is already in the database',
-            )
-    options, args = parser.parse_args()
-    dataset = options.dataset_name
-    analysis = options.analysis_name
-    force_import = options.force_import
-    state_file = options.state_file
-    add_metric(dataset, analysis, force_import, state_file=state_file)
-
-# vim: et sw=4 sts=4
