faf6b76e2cafd6979ad3a6f5c024625c462a28bc
Josh Hansen <hansen.joshuaa@gmail.com>
2012-03-20 23:40:37 -0600

    Fix word in context error in which the left context was stretching past the beginning of the document

--- topic_modeling/visualize/ajax_calls.py
+++ topic_modeling/visualize/ajax_calls.py
@@ -58,7 +58,7 @@ def word_in_context(request, dataset, analysis, word, topic=None):
     
     token = tokens[random.randint(0, len(tokens)-1)]
     doc = token.doc
-    context = token.doc.tokens.all()[token.token_index-5:token.token_index+5]
+    context = doc.tokens.all()[max(0,token.token_index-5):token.token_index+5]
     
     word_in_context = dict()
     word_in_context['word'] = token.type.type
