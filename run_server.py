30839c71f23984ec278e670165219421f74dc3a3
Jared Forsyth <jared@jaredforsyth.com>
2013-04-02 14:35:37 -0600

    adding run_server.py to replace the .sh file. Also: gunicorn integration.

---
+++ run_server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from topic_modeling import manage
+
+def run(cmd):
+    manage.execute_manager(manage.settings, ['run_server.py', cmd])
+
+def main():
+    if manage.settings.local_settings.USE_GUNICORN:
+        if manage.settings.gunicorn:
+            return run('run_gunicorn')
+        print "Gunicorn not installed; defaulting to normal execution"
+    if manage.settings.django_extensions:
+        return run('runserver_plus')
+    return run('runserver')
+
+
+if __name__ == '__main__':
+    main()
