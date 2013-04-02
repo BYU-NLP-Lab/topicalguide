66391447224ba3a7651029bc65847a360b8b65ca
Jared Forsyth <jared@jaredforsyth.com>
2013-04-02 10:57:13 -0600

    style merge

--- topic_modeling/media/scripts/datasets.js
+++ topic_modeling/media/scripts/datasets.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
+    /*
     $("#accordion").accordion({autoHeight: false, collapsible: true, active: false})
                    .bind('accordionchange', function(event, ui) {
                       update_plot(ui.newHeader.attr('dataset_name'));
                    });
+                   */
     
 
     //Event handlers
@@ -14,20 +16,20 @@ $(document).ready(function() {
 });
 
 function explore() {
-	var dataset = _dataset_node($(this));
-	window.location.href = $("select.analysis > option:selected", dataset).attr("url");
+    var dataset = _dataset_node($(this));
+    window.location.href = $("select.analysis > option:selected", dataset).attr("url");
 }
 
 function load_plot_image() {
-	var dataset = _dataset_node($(this));
-	var img = $("img.plot", dataset);
-	var selected = $("select.analysis > option:selected", dataset);
-	img.attr('src', selected.attr('img_url'));
+    var dataset = _dataset_node($(this));
+    var img = $("img.plot", dataset);
+    var selected = $("select.analysis > option:selected", dataset);
+    img.attr('src', selected.attr('img_url'));
 }
 
 function _dataset_node(element) {
-	while(!element.attr('dataset')) {
-		element = element.parent();
-	}
-	return element;
+    while(!element.attr('dataset')) {
+        element = element.parent();
+    }
+    return element;
 }
