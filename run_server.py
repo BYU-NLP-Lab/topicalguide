7ff5b4c28bb50d364f844af0514a3b90bf7d93e9
Chris Tensmeyer <christopher.tensmeyer@gmail.com>
2013-04-11 13:27:01 -0600

    For merging

--- run_server.py
+++ run_server.py
@@ -6,10 +6,12 @@ def run(cmd):
     manage.execute_manager(manage.settings, ['run_server.py', cmd])
 
 def main():
+    '''
     if manage.settings.local_settings.USE_GUNICORN:
         if manage.settings.gunicorn:
             return run('run_gunicorn')
         print "Gunicorn not installed; defaulting to normal execution"
+    '''
     if manage.settings.django_extensions:
         return run('runserver_plus')
     return run('runserver')
