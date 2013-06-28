502c6d8fde0f784b1c83c28a3c0d9f4b3cc0590b
Chris Tensmeyer <christopher.tensmeyer@gmail.com>
2013-06-27 20:55:19 -0600

    Added size threshold slider for force diagram

--- topic_modeling/media/scripts/charts/force.js
+++ topic_modeling/media/scripts/charts/force.js
@@ -1,3 +1,4 @@
+//TODO: You need to understand a lot more of whats going on here...
 
 /**
  * Create a bootstrap button group
@@ -21,8 +22,10 @@ function translate_node(d) {
   return 'translate(' + d.x + ',' + d.y + ')'; 
 }
 
+//performance fix
 function twofloat(n) {
-  return parseInt(n * 100)/100.0;
+  //return parseInt(n * 100)/100.0;
+  return Math.round(n * 100) / 100.0;
 }
 
 /**
@@ -58,20 +61,41 @@ function get_topicsminmax(topics) {
   return [min, max];
 }
 
-function force_links(nodes, matrix, threshhold) {
+/* returns an array of link objects {source : <index>, target: <index>, weight: <float>}
+    where the size of the source and target nodes are within the size threshold and
+    the link weight is within the link threshold
+*/
+function force_links(nodes, matrix, lk_thresh, sz_thresh, scale) {
   /** create links for the force layout **/ 
-  var links = [], target;
-  for (var i=0; i<nodes.length; i++) {
-    for (var j=0; j<nodes[i].topics.length; j++) {
-      target = nodes[i].topics[j];
-      if (matrix[i][target] <= threshhold[1] && matrix[i][target] >= threshhold[0])
-        links.push({source: i, target: target, weight: matrix[i][target]});
+  var links = [], target, target_node, src_node;
+  for (var src=0; src<nodes.length; src++) {
+
+    src_node = nodes[src];
+    if(!in_range(src_node, scale, sz_thresh)) {
+      continue;
+    }
+
+    for (var j=0; j<nodes[src].topics.length; j++) {
+      target = nodes[src].topics[j];
+      target_node = nodes[target];
+
+      if (in_range(target_node, scale, sz_thresh) && 
+          matrix[src][target] <= lk_thresh[1] &&
+          matrix[src][target] >= lk_thresh[0]) {
+            links.push({source: src_node, target: target_node, weight: matrix[src][target]});
+            //console.log(src + " to " + target);
+          }
     }
   }
   return links;
 }
 
-function make_rel(minmax) {
+function in_range(node, scale, thresh) {
+  var size = scale(node.metrics['Number of tokens']);
+  return size <= thresh[1] && size >= thresh[0];
+}
+
+function make_scale(minmax) {
   var rng = minmax[1] - minmax[0];
   return function (x) {
     return (x - minmax[0]) / rng;
@@ -118,6 +142,9 @@ var ForceInfo = InfoView.extend({
   },
 
   load_topic: function (id, info, metrics) {
+    console.log(id);
+    console.log(info);
+    console.log(metrics);
     this.$el.hide();
     // get the popoverrolling
     var details_url = location.href.split('/').slice(0,-1).join('/') + '/topics/' + id;
@@ -127,9 +154,14 @@ var ForceInfo = InfoView.extend({
     this.$('.topic-name').text(info.names[0]);
     var mtable = this.$('table.metrics tbody');
     mtable.empty();
+
     _.forOwn(info.metrics, function (value, key) {
-        mtable.append('<tr><td>' + key + '</td><td>' + twofloat(value) + '</td><td>' + twofloat(metrics[key].min) + '</td><td>' + twofloat(metrics[key].max) + '</td></tr>');
+        mtable.append('<tr><td>' + key + '</td><td>' +
+                        twofloat(value) + '</td><td>' +
+                        twofloat(metrics[key].min) + '</td><td>' +
+                        twofloat(metrics[key].max) + '</td></tr>');
     });
+
     var dtable = this.$('table.documents tbody');
     dtable.empty();
     _.each(info.documents, function (doc, i) {
@@ -155,15 +187,27 @@ var ForceInfo = InfoView.extend({
 var ForceControls = Backbone.View.extend({
   initialize: function (options) {
     var parent = this.parent = options.parent;
-    var t = parent.options.force_threshold;
-    this.$('.threshhold').slider({
+    var link_th = parent.options.link_threshold;
+    this.$('#force-link-slider').slider({
       range: true,
       min: 0,
       max: 1,
-      values: parent.options.force_threshold,
-      step: (t[1] - t[0]) / 20,
+      values: parent.options.link_threshold,
+      step: (link_th[1] - link_th[0]) / 20,
       stop: function ( event, ui ) {
-        parent.set_threshhold(ui.values);
+        parent.set_link_th(ui.values);
+      }
+    });
+
+    var size_th = parent.options.size_threshold;
+    this.$('#force-size-slider').slider({
+      range: true,
+      min: 0,
+      max: 1,
+      values: size_th,
+      step: (size_th[1] - size_th[0]) / 20,
+      stop: function ( event, ui ) {
+        parent.set_size_th(ui.values);
       }
     });
   },
@@ -195,9 +239,12 @@ var ForceViewer = MainView.add(ZoomableView, {
     max_link_strength: .1,
     line_width: 3,
     full_scale: 3,
-    force_threshold: [0.7, 1],
+    link_threshold: [0.7, 1],
+    size_threshold: [0.4, 1],
     pairwise: 'document correlation'
   },
+
+  first : true,
 
   initialize: function () {
     VisualizationView.prototype.initialize.apply(this, arguments);
@@ -217,18 +264,32 @@ var ForceViewer = MainView.add(ZoomableView, {
 
   load: function (data) {
     this.data = data;
-    var nodes = data.topics;
-    var links = force_links(nodes, data.matrix, this.options.force_threshold);
+
+    this.all_nodes = data.topics;
+    /* this loop assigns the absolute index (into data.topics) to each node, so when
+        we filter and use a subset, we can properly populate the info panel.  d3.force uses
+        node.index so we use node.abs_idx */
+    for(var k = 0; k < this.all_nodes.length; k++) {
+      this.all_nodes[k].abs_idx = k;
+    }
+
+    var tminmax = get_topicsminmax(this.all_nodes);
+    this.node_scale = make_scale(tminmax);
+    this.nodes = this.filter_nodes(this.all_nodes, this.node_scale, this.options.size_threshold);
+
     var minmax = get_matrixminmax(data.matrix);
-    var tminmax = get_topicsminmax(data.topics);
-    var rel = make_rel(minmax);
-    var trel = make_rel(tminmax);
+    if (minmax[0] < this.options.link_threshold[0]) minmax[0] = this.options.link_threshold[0];
+    if (minmax[1] > this.options.link_threshold[1]) minmax[1] = this.options.link_threshold[1];
+    var link_scale = this.link_scale = make_scale(minmax);
+    var links = force_links(this.all_nodes, data.matrix, this.options.link_threshold,
+                            this.options.size_threshold, this.node_scale);
+
     var that = this;
     this.info.clear();
     // populate the force layout
-    this.force.nodes(nodes).links(links)
+    this.force.nodes(this.nodes).links(links)
               .linkStrength(function (d) {
-                return that.options.max_link_strength * rel(d.weight);
+                return that.options.max_link_strength * link_scale(d.weight);
               }).start();
     // this.calc_layout();
 
@@ -236,11 +297,17 @@ var ForceViewer = MainView.add(ZoomableView, {
     this.maing.append('g').classed('all-links', true);
     this.maing.append('g').classed('all-nodes', true);
     // create links first so they're behind everything
-    this.create_links(links, rel);
-    this.create_nodes(nodes, data, trel);
+    this.create_links(links, link_scale);
+    this.create_nodes(this.nodes, this.data, this.node_scale);
     // update the positions of all the created nodes
     // this.update_positions();
     this.start_ticking();
+
+    //first load should zoom in to see...
+    if (this.first) {
+      this.zoom_in.on('click')();
+      this.first = false;
+    }
   },
 
   create_links: function (links, rel) {
@@ -258,16 +325,17 @@ var ForceViewer = MainView.add(ZoomableView, {
     this.link.remove();
   },
 
-  set_threshhold: function (threshhold) {
+  set_link_th: function (threshold_lk) {
     var data = this.data;
-    this.options.force_threshold = threshhold;
+    this.options.link_threshold = threshold_lk;
     var minmax = get_matrixminmax(data.matrix);
-    if (minmax[0] < threshhold[0]) minmax[0] = threshhold[0];
-    if (minmax[1] > threshhold[1]) minmax[1] = threshhold[1];
-    var rel = make_rel(minmax);
+    if (minmax[0] < threshold_lk[0]) minmax[0] = threshold_lk[0];
+    if (minmax[1] > threshold_lk[1]) minmax[1] = threshold_lk[1];
+    var scale = make_scale(minmax);
     this.remove_links();
-    var links = force_links(data.topics, data.matrix, this.options.force_threshold);
-    this.create_links(links, rel);
+    var links = force_links(this.all_nodes, data.matrix, this.options.link_threshold,
+                            this.options.size_threshold, this.node_scale);
+    this.create_links(links, scale);
     if (this.ticking) {
       this.force.stop()
       this.force.links(links);
@@ -279,7 +347,24 @@ var ForceViewer = MainView.add(ZoomableView, {
     }
   },
 
-  create_nodes: function (nodes, data, trel) {
+  set_size_th: function (threshold_sz) {
+    this.options.size_threshold = threshold_sz;
+    this.force.stop();
+    this.load(this.data);
+  },
+
+  filter_nodes: function (nodes, scale, threshold) {
+    var filtered = [];
+    for(var k = 0; k < nodes.length; k++) {
+      var node = nodes[k];
+      if(in_range(node, scale, threshold)) {
+         filtered.push(nodes[k]);
+      }
+    }
+    return filtered;
+  },
+
+  create_nodes: function (nodes, data, scale) {
     var that = this;
     this.node = this.maing.select('g.all-nodes').selectAll("g.node")
         .data(nodes)
@@ -287,7 +372,7 @@ var ForceViewer = MainView.add(ZoomableView, {
         .attr('class', function (d, i) { return 'node node-' + i; })
         .attr('pointer-events', 'all')
         .on('click', function (d, i) {
-          that.node_click(d, this, data.topics[d.index]);
+          that.node_click(d, this, data.topics[d.abs_idx]);
         }).call(this.force.drag)
         .on('mouseover', function (d, i) {
           d3.select('text.node-text-' + i)
@@ -296,7 +381,7 @@ var ForceViewer = MainView.add(ZoomableView, {
           d3.select('text.node-text-' + i)
             .transition().attr('transform', 'translate(0, -3) scale(1)');
         });
-    this.create_circles(trel);
+    this.create_circles(scale);
     this.create_texts(nodes);
   },
 
@@ -343,7 +428,8 @@ var ForceViewer = MainView.add(ZoomableView, {
 
   /* click callback for the nodes */
   node_click: function (data, node, topic) {
-    this.info.load_topic(data.index, topic, this.data.metrics);
+
+    this.info.load_topic(data.abs_idx, topic, this.data.metrics);
     var s = this.options.full_scale;
     this.zoom.translate([-s*data.x + this.options.width/2,
                         -s*data.y + this.options.height/2]).scale(s);
