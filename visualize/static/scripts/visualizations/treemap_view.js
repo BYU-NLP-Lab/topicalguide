3495164b778a97c484cc39e3d17ed375711384f6
Craig Jacobson <craig.crj@gmail.com>
2015-05-28 14:56:55 -0600

    Create basic setup for treemap view.

---
+++ visualize/static/scripts/visualizations/treemap_view.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var TreemapView = DefaultView.extend({
+    
+    readableName: "Treemap",
+    shortName: "treemap",
+    
+    redirectTemplate:
+'<div class="text-center">'+
+'   <button class="treemap-redirect btn btn-default">'+
+'       <span class="glyphicon glyphicon-chevron-left pewter"></span> Datasets'+
+'   </button>'+
+'   <span> You need to select a dataset and analysis before using this view. </span>'+
+'</div>',
+
+    baseTemplate:
+'TREEEeeeEEEee Map View....',
+    
+    initialize: function initialize() {
+        this.listenTo(this.selectionModel, "change:analysis", this.render);
+    },
+    
+    cleanup: function cleanup() {},
+    
+    render: function render() {
+        if(!this.selectionModel.nonEmpty(["dataset", "analysis"])) {
+            this.$el.html(this.redirectTemplate);
+        } else {
+            this.$el.html(this.baseTemplate);
+        }
+    },
+    
+    events: {
+        "click .treemap-redirect": "clickRedirect",
+    },
+    
+    clickRedirect: function clickRedirect() {
+        this.viewModel.set({ currentView: "datasets" });
+    },
+    
+});
+
+addViewClass([], TreemapView);
