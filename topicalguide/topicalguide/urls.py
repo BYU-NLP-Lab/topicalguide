e48c564e972cff0dcf4672cf73fca55a24637135
root <root@coherence.aml.cs.byu.edu>
2016-02-05 12:34:38 -0700

    Committing to allow a pull to allow a push

--- topicalguide/topicalguide/urls.py
+++ topicalguide/topicalguide/urls.py
@@ -5,5 +5,4 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('visualize.urls')),
-    #url(r'^', include('abtest.urls')),
 )
