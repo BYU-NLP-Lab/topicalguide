924a93b55d03239ad125cf7e7a1732d8168eec0d
madisonjbrooks12 <madisonjbrooks12@gmail.com>
2016-06-14 14:32:54 -0600

    Removed contracts

--- default_datasets/import_contracts.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-python tg.py import default_datasets/contract_dataset --public \
---public-documents --identifier legal_contracts --verbose
-python tg.py analyze legal_contracts --verbose --subdocuments --number-of-topics 100 --stopwords stopwords/english_all.txt --remove-singletons --stem-words --bigrams
-
-
